0adc5c26fded6e0787d03472b7e4d7f879af2822
Dominik Habersack <dom@islovely.co>
2014-07-30 14:57:54 +0200

    Replace Link with Turtle, make him react to left and right arrow-keys.

--- js/states/play-state.js
+++ js/states/play-state.js
@@ -1,17 +1,18 @@
 var PlayState = {
-  link: null,
+  turtle: null,
   textLabel: null,
 
   preload: function() {
     this.load.image('forest-tiles', '/img/tiles/forest.png');
 
-    this.load.spritesheet('link', '/img/sprites/link.png', 24, 24);
+    this.load.spritesheet('turtle', '/img/sprites/turtle.png', 24, 24);
 
     this.load.tilemap('forest-tilemap', '/img/tiles/forest.json', null, Phaser.Tilemap.TILED_JSON);
   },
 
   create: function() {
-    var layer,
+    var cursorKeys,
+        layer,
         tilemap;
 
     tilemap = this.add.tilemap('forest-tilemap');
@@ -21,50 +22,41 @@ var PlayState = {
     layer = tilemap.createLayer('layer-1');
     layer.resizeWorld();
 
-    this.link = new Link(this.game, 9, 5, 0);
+    this.turtle = new Turtle(this.game, 1, 8, 0);
 
-    this.textLabel = this.add.text(10, 10, 'textLabel', { 'font': '24px Lato' });
+    this.textLabel = this.add.text(10, 10, 'textLabel');
     this.textLabel.fixedToCamera = true;
 
     this.physics.startSystem(Phaser.Physics.ARCADE);
 
+    cursorKeys = this.input.keyboard.createCursorKeys();
+
     this.input.keyboard.addKeyCapture([
-        Phaser.Keyboard.UP,
-        Phaser.Keyboard.DOWN,
-        Phaser.Keyboard.LEFT,
-        Phaser.Keyboard.RIGHT
+        cursorKeys.up,
+        cursorKeys.down,
+        cursorKeys.Left,
+        cursorKeys.right
     ]);
 
-    var upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
-    upKey.onDown.add(this.link.turnNorth, this.link);
-
-    var downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-    downKey.onDown.add(this.link.turnSouth, this.link);
-
-    var rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-    rightKey.onDown.add(this.link.turnEast, this.link);
-
-    var leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-    leftKey.onDown.add(this.link.turnWest, this.link);
+    cursorKeys.right.onDown.add(this.turtle.turnRight, this.turtle);
+    cursorKeys.left.onDown.add(this.turtle.turnLeft, this.turtle);
   },
 
   update: function() {
-    // this.checkKeys();
+    this.checkKeys();
   },
 
-  // checkKeys: function() {
-  //   var cursorKeys = this.input.keyboard.createCursorKeys();
+  checkKeys: function() {
+    var cursorKeys;
 
-  //   if (cursorKeys.up.isDown) {
-  //   }
+    cursorKeys = this.input.keyboard.createCursorKeys();
 
-  //   if (cursorKeys.down.isDown) {
-  //   }
+    if (cursorKeys.left.isDown) {
+      this.turtle.moveLeft();
+    }
 
-  //   if (cursorKeys.left.isDown) {
-  //   }
-
-  //   if (cursorKeys.right.isDown) {
-  //   }
-  // }
+    if (cursorKeys.right.isDown) {
+      this.turtle.moveRight();
+    }
+  }
 };
