e3263e74366e8c688809ab2e28eeb18f25cd26b3
Miriam Mayer <miri.mayer@t-online.de>
2014-08-07 22:56:59 +0200

    New music
    dying when hazardous terrain/water horrible

--- js/states/preload-state.js
+++ js/states/preload-state.js
@@ -7,6 +7,7 @@ var PreloadState = {
     game.load.audio('aua', 'music/aua.mp3');
     game.load.audio('backgroundmusic', 'music/backgroundmusic.mp3');
     game.load.audio('dring', 'music/dring.mp3');
+    game.load.audio('dying', 'music/dying.mp3');
     game.load.audio('gameover', 'music/gameover.mp3');
     game.load.audio('gulp', 'music/gulp.mp3');
     game.load.audio('menu', 'music/menu.mp3');
