d3b27d2359146b588a2cb0e3a4afe2aaee58f77e
Miriam Mayer <miri.mayer@t-online.de>
2014-08-07 10:58:22 +0200

    goodies

--- js/config/levels/level-4-1.js
+++ js/config/levels/level-4-1.js
@@ -10,6 +10,59 @@ var levelFourOne = {
     },
     'height': 8
   },
+  'goodies': [
+    {
+      'goody': 'bubble',
+      'positions': [
+        {
+          'x': 37,
+          'y': 2
+        },
+        {
+          'x': 108,
+          'y': 8
+        },
+        {
+          'x': 117,
+          'y': 8
+        },
+        {
+          'x': 126,
+          'y': 8
+        }
+      ]
+    },
+    {
+      'goody': 'chili',
+      'positions': [
+        {
+          'x': 10,
+          'y': 8
+        },
+        {
+          'x': 49,
+          'y': 9
+        },
+        {
+          'x': 64,
+          'y': 8
+        }
+      ]
+    },
+    {
+      'goody': 'strawberry',
+      'positions': [
+        {
+          'x': 46,
+          'y': 1
+        },
+        {
+          'x': 104,
+          'y': 1
+        }
+      ]
+    }
+  ],
   'player': {
     'jumpVelocity' : -400,
     'walkDrag' : 800,
